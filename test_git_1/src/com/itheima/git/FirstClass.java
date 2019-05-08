79a15af036a604886ee84609f921aa377cde1e72
bruce <chenkuobang@163.com>
2019-05-08 12:02:05 +0800

    添加本地工程到git

---
+++ test_git_1/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println(" first git project in the world!");
+        System.out.println(" second git project in the world!");
+        System.out.println(" third git project in the world!");
+    }
+
+}
